7a53556d359ade553074ee584b1f77271ba56c2c
Ethan Rublee <ethan.rublee@gmail.com>
2020-07-31 00:41:52 -0700

    WIP, camera integration.

--- python/farm_ng/tracking_camera.py
+++ python/farm_ng/tracking_camera.py
@@ -8,7 +8,11 @@ from farm_ng_proto.tractor.v1.tracking_camera_pb2 import TrackingCameraPoseFrame
 from google.protobuf.timestamp_pb2 import Timestamp
 from google.protobuf.text_format import MessageToString
 import time
+import os
 import sys
+import concurrent.futures
+from subprocess import Popen, PIPE
+import io
 
 logger = logging.getLogger('tracking_camera')
 plog = farm_ng.proio_utils.get_proio_logger()
@@ -31,29 +35,57 @@ _confidence_map = {
     3: TrackingCameraPoseFrame.CONFIDENCE_HIGH,
 }
 
+class VideoEncoder(object):
+    def __init__(self, intrinsics, out_path, framerate=30):
+        print(intrinsics)
+        cmd="gst-launch-1.0 fdsrc fd=0 ! videoparse width=%d height=%d framerate=10/1 format=gray8 ! videoconvert ! omxh264enc preset-level=0 bitrate=1000000 ! video/x-h264, stream-format=byte-stream ! rtph264pay mtu=1400 ! udpsink host=228.0.0.10 port=5000 sync=false async=false"%(intrinsics.width, intrinsics.height)
+
+        # to watch this remotely run:
+        # gst-launch-1.0 udpsrc multicast-group=228.0.0.10 port=5000 ! application/x-rtp,encoding-name=H264,payload=96 ! rtph264depay ! h264parse ! queue ! avdec_h264 ! xvimagesink sync=false async=false -e
+        
+        print('running subprocess: %s'%cmd)
+        self.converter = Popen(cmd,
+            stdin=PIPE, stdout=PIPE, # stderr=io.open(os.devnull, 'wb'),
+            shell=True, close_fds=True)
+
+    def write(self, b):
+        self.converter.stdin.write(b)
+
+    def flush(self):
+        self.converter.stdin.close()
+        self.converter.wait()
+
 
 class TrackingCamera:
     def __init__(self, name):
         self.name = name
         self.loop = asyncio.get_event_loop()
         self.pipe = rs.pipeline()
+        self.encoder = None
         self.running = False
+        self.image_count = 0
         self.recording_number = 0
         self.image_data = {"left"  : None,
                            "right" : None,
                            "timestamp_ms" : None}
         
         self.pose_frame = TrackingCameraPoseFrame()
-
+        
     def on_pose_frame(self, pose_frame):
         self.pose_frame = pose_frame
         event = plog.make_event({'%s/pose' % self.name: pose_frame})
         plog.writer().push(event)
-        if pose_frame.frame_number % 200 == 0:
+        if False and pose_frame.frame_number % 200 == 0:
             logger.info(MessageToString(pose_frame, as_one_line=True))
 
     def on_image_data(self, image_data):
         self.image_data = image_data
+        if self.encoder is not None:
+            self.encoder.write(image_data['left'])
+            
+            #stack = np.stack((image_data['left'], image_data['right'],image_data['right']),
+            #axis=-1)
+            #self.encoder.write(stack) #image_data['left'])
         # TODO(rublee) log with H264 encoding
         #event = plog.make_event({'%s/image_data' % self.name: image_data})
         #plog.writer().push(event)
@@ -67,25 +99,68 @@ class TrackingCamera:
     def stop_pipeline(self):
         if self.running:
             self.pipe.stop()
+            if self.encoder is not None:
+                self.encoder.flush()
+                self.encoder = None
             self.running= False
-        
+
     def start_pipeline(self, record):
         self.stop_pipeline()
         cfg = rs.config()
         bag_path = None
-        if record:
-            self.recording_number += 1
-            bag_path = os.path.join(plog.log_dir, '%03d.bag'%self.recording_number)
-            logger.info('recording: %s path: %s'%(record, bag_path))
-            cfg.enable_all_streams()
-            cfg.enable_record_to_file(bag_path)
-        else:
-            cfg.enable_stream(rs.stream.pose)
-            logger.info('Enabling pose stream only.')
 
+        cfg.enable_all_streams()
+        #cfg.enable_stream(rs.stream.pose)                    
+
+        profile = cfg.resolve(self.pipe)
+
+        
+        streams = {"left"  : profile.get_stream(rs.stream.fisheye, 1).as_video_stream_profile(),
+                     "right" : profile.get_stream(rs.stream.fisheye, 2).as_video_stream_profile()}
+        intrinsics = {"left"  : streams["left"].get_intrinsics(),
+                        "right" : streams["right"].get_intrinsics()}
+        print(intrinsics)
+        if record:
+             self.recording_number += 1
+             video_path = os.path.join(plog.log_dir, 'left_%03d.mp4'%self.recording_number)
+             self.encoder = VideoEncoder(intrinsics['left'], video_path)
+
+        dev = profile.get_device()
+        tm2 = dev.as_tm2()
+        pose_sensor = tm2.first_pose_sensor()
+        pose_jumping = pose_sensor.get_option(rs.option.enable_pose_jumping)
+        reloc = pose_sensor.get_option(rs.option.enable_relocalization)
+        print('pose jumping: %s'%pose_jumping)
+        print('relocalization: %s'%reloc)
+        pose_sensor.set_option(rs.option.enable_pose_jumping, False)
+        pose_sensor.set_option(rs.option.enable_relocalization, False)
+        pose_jumping = pose_sensor.get_option(rs.option.enable_pose_jumping)
+        reloc = pose_sensor.get_option(rs.option.enable_relocalization)
+        print('pose jumping: %s'%pose_jumping)
+        print('relocalization: %s'%reloc)
+
+        # if record:
+        #     logger.info('recording: %s path: %s'%(record, bag_path))
+        #     #cfg.enable_stream(rs.stream.pose)
+        #     #cfg.enable_all_streams() # too much data
+        #     cfg.enable_record_to_file(bag_path)
+        # else:
+        #     #cfg.enable_stream(rs.stream.pose)
+        #     logger.info('Enabling pose stream only.')
 
         # Start streaming with requested config
         self.pipe.start(cfg, self.frame_callback)
+
+        # setting options for slam:
+        # https://github.com/IntelRealSense/librealsense/issues/1011
+        # and what to set:
+        # https://github.com/IntelRealSense/realsense-ros/issues/779 "
+        # I would suggest leaving mapping enabled, but disabling
+        # relocalization and jumping. This may avoid the conflict with
+        # RTabMap while still giving good results."
+        #
+        # https://intelrealsense.github.io/librealsense/python_docs/_generated/pyrealsense2.option.html#pyrealsense2.option
+        #
         self.running = True
         
 
@@ -97,19 +172,24 @@ class TrackingCamera:
             
     def _frame_callback(self, frame):
         if frame.is_frameset():
-            frameset = frame.as_frameset()
-            ts = frameset.get_timestamp()
-            f1 = frameset.get_fisheye_frame(1).as_video_frame()
-            f2 = frameset.get_fisheye_frame(2).as_video_frame()
-            left_data = np.asanyarray(f1.get_data())
-            right_data = np.asanyarray(f2.get_data())
-            image_data = dict(left=left_data, right=right_data, timestamp_ms=ts)
-            self.loop.call_soon_threadsafe(self.on_image_data, image_data)
+            self.image_count = (self.image_count  + 1)% 3
+            if self.image_count == 0:
+                frameset = frame.as_frameset()
+                ts = frameset.get_timestamp()
+                f1 = frameset.get_fisheye_frame(1).as_video_frame()
+                f2 = frameset.get_fisheye_frame(2).as_video_frame()
+                left_data = np.asanyarray(f1.get_data())
+                right_data = np.asanyarray(f2.get_data())
+                image_data = dict(left=left_data, right=right_data, timestamp_ms=ts)
+                self.loop.call_soon_threadsafe(self.on_image_data, image_data)
+                
         # Fetch pose frame
         pose = frame.is_pose_frame()
         if pose:
             # Print some of the pose data to the terminal
             frame = frame.as_pose_frame()
+            if frame.frame_number % 4 != 0:
+                return
             data = frame.get_pose_data()
             pose_frame = TrackingCameraPoseFrame()
             pose_frame.frame_number = frame.frame_number
@@ -126,14 +206,18 @@ class TrackingCamera:
 
         
 
-
 def main():
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
     event_loop = asyncio.get_event_loop()
     camera = TrackingCamera('tracking_camera/front')
-    camera.start_pipeline(record=False)
+    camera.start_pipeline(record=True)
     _ = Periodic(60, event_loop, lambda n_periods: plog.writer().flush())
-    event_loop.run_forever()
+    #_ = Periodic(100, event_loop, lambda n_periods: camera.start_pipeline(True))
+    try:
+        event_loop.run_forever()
+    except KeyboardInterrupt:
+        camera.stop_pipeline()
+
 
 
 if __name__ == '__main__':
--- python/farm_ng/tractor/driver/__main__.py
+++ python/farm_ng/tractor/driver/__main__.py
@@ -12,135 +12,12 @@ from farm_ng.periodic import Periodic
 from google.protobuf.text_format import MessageToString
 from farm_ng.tractor.kinematics import TractorKinematics
 import numpy as np
-import pyrealsense2 as rs
 
 logger = logging.getLogger('tractor.driver')
 plog = farm_ng.proio_utils.get_proio_logger()
 
 kinematics = TractorKinematics()
 
-rs_count = 0
-
-def frame_callback(frame):
-    global rs_count
-    # print(frame, frame.is_pose_frame(), frame.is_frameset())
-    if frame.is_frameset():
-        frameset = frame.as_frameset()
-        f1 = frameset.get_fisheye_frame(1).as_video_frame()
-        f2 = frameset.get_fisheye_frame(2).as_video_frame()
-        left_data = np.asanyarray(f1.get_data())
-        right_data = np.asanyarray(f2.get_data())
-        #print(left_data.shape, right_data.shape)
-    # Fetch pose frame
-    pose = frame.is_pose_frame()
-    if pose:
-        # Print some of the pose data to the terminal
-        frame = frame.as_pose_frame()
-        data = frame.get_pose_data()
-        rs_count += 1
-        if rs_count % 30 == 0:
-            print(f'Frame #{frame.frame_number} Position: {data.translation} Velocity: {data.velocity}')
-
-
-class TrackingCamera:
-    def __init__(self):
-        self.pipe = rs.pipeline()
-        self.running = False
-        self.recording_number = 0
-        self.frame_mutex = threading.Lock()
-        self.image_data = {"left"  : None,
-                           "right" : None,
-                           "timestamp_ms" : None}
-        self.pose_data = {"translation" : None,
-                          "velocity": None,
-                          "rotation": None,
-                          "angular_velocity": None,
-                          "angular_acceleration": None,
-                          "tracker_confidence": None,
-                          "mapper_confidence": None,
-                          "timestamp_ms": None}
-
-    def get_pose_data(self):
-        with self.frame_mutex:
-            return self.pose_data
-
-    def stop_pipeline(self):
-        if self.running:
-            self.pipe.stop()
-            self.running= False
-        
-    def start_pipeline(self, record):
-        self.stop_pipeline()
-
-        cfg = rs.config()
-        cfg.enable_all_streams()
-
-        bag_path = None
-        if record:
-            self.recording_number += 1
-            bag_path = os.path.join(plog.log_dir, '%03d.bag'%self.recording_number)
-            cfg.enable_record_to_file(bag_path)
-
-        logger.info('recording: %s path: %s'%(record, bag_path))
-        # Start streaming with requested config
-        self.pipe.start(cfg, self.frame_callback)
-        self.running = True
-        
-
-    def frame_callback(self, frame):
-        try:
-            self._frame_callback(frame)
-        except Exception as e:
-            print(e)
-            
-    def _frame_callback(self, frame):
-        global rs_count
-        # print(frame, frame.is_pose_frame(), frame.is_frameset())
-        if frame.is_frameset():
-            frameset = frame.as_frameset()
-            ts = frameset.get_timestamp()
-            f1 = frameset.get_fisheye_frame(1).as_video_frame()
-            f2 = frameset.get_fisheye_frame(2).as_video_frame()
-            left_data = np.asanyarray(f1.get_data())
-            right_data = np.asanyarray(f2.get_data())
-            with self.frame_mutex:
-                self.image_data['left'] = left_data
-                self.image_data['right'] = right_data
-                self.image_data['timestamp_ms'] = ts
-            
-
-            #print(left_data.shape, right_data.shape)
-        # Fetch pose frame
-        pose = frame.is_pose_frame()
-        if pose:
-            # Print some of the pose data to the terminal
-            frame = frame.as_pose_frame()
-            ts = frame.get_timestamp()
-            ts_real = time.time()*1000
-            print(ts - ts_real)
-            data = frame.get_pose_data()
-            '''
-            .def_readwrite("translation", &rs2_pose::translation, "X, Y, Z values of translation, in meters (relative to initial position)")
-        .def_readwrite("velocity", &rs2_pose::velocity, "X, Y, Z values of velocity, in meters/sec")
-        .def_readwrite("acceleration", &rs2_pose::acceleration, "X, Y, Z values of acceleration, in meters/sec^2")
-        .def_readwrite("rotation", &rs2_pose::rotation, "Qi, Qj, Qk, Qr components of rotation as represented in quaternion rotation (relative to initial position)")
-        .def_readwrite("angular_velocity", &rs2_pose::angular_velocity, "X, Y, Z values of angular velocity, in radians/sec")
-        .def_readwrite("angular_acceleration", &rs2_pose::angular_acceleration, "X, Y, Z values of angular acceleration, in radians/sec^2")
-        .def_readwrite("tracker_confidence", &rs2_pose::tracker_confidence, "Pose confidence 0x0 - Failed, 0x1 - Low, 0x2 - Medium, 0x3 - High")
-        .def_readwrite("mapper_confidence", &rs2_pose::mapper_confidence, "Pose map confidence 0x0 - Failed, 0x1 - Low, 0x2 - Medium, 0x3 - High");
-        '''
-            with self.frame_mutex:
-                self.pose_data['translation'] = data.translation
-                self.pose_data['velocity'] = data.velocity
-                self.pose_data['acceleration'] = data.acceleration
-                self.pose_data['rotation'] = data.rotation
-                self.pose_data['angular_velocity'] = data.angular_velocity
-                self.pose_data['angular_acceleration'] = data.angular_acceleration
-                self.pose_data['tracker_confidence'] = data.tracker_confidence
-                self.pose_data['mapper_confidence'] = data.mapper_confidence
-                self.pose_data['timestamp_ms'] = ts
-
-        
 class TractorController:
     def __init__(self, event_loop):
         self.event_loop = event_loop
@@ -149,11 +26,9 @@ class TractorController:
         self.n_cycle = 0
         self.speed = 0.0
         self.angular = 0.0
-        self.record_counter = 0
-        self.record_toggle = False
+        # self.record_counter = 0
+        # self.recording = False
         self.lock_out = False
-        self.tracking_camera = TrackingCamera()
-        self.tracking_camera.start_pipeline(record=False)
         self.can_socket = CANSocket('can0', self.event_loop)
         self.joystick = MaybeJoystick('/dev/input/js0', self.event_loop)
 
@@ -171,30 +46,20 @@ class TractorController:
 
         self.control_timer = Periodic(
             self.command_period_seconds, self.event_loop,
-            self._command_loop,
+            self._command_loop, name='control_loop'
         )
 
     def _command_loop(self, n_periods):
-        if (self.n_cycle % (2*self.command_rate_hz)) == 0:
-            logger.info('\nright motor:\n  %s\nleft motor:\n  %s\npose:\n  %s',
+        if (self.n_cycle % (5*self.command_rate_hz)) == 0:
+            logger.info('\nright motor:\n  %s\nleft motor:\n  %s\n',
                         MessageToString(self.right_motor.get_state(), as_one_line=True),
-                        MessageToString(self.left_motor.get_state(), as_one_line=True),
-                        self.tracking_camera.get_pose_data())
+                        MessageToString(self.left_motor.get_state(), as_one_line=True))
 
-        if self.joystick.get_button_state('b', False) and self.record_counter == 0:
-            self.record_counter = 200
-            self.record_toggle = not self.record_toggle
-            self.tracking_camera.start_pipeline(self.record_toggle)
-        if self.record_counter > 0:
-            self.record_counter -= 1
-        
-            
         self.n_cycle += 1
 
         brake_current=10.0
         
-        if ( n_periods*self.command_period_seconds >= 0.25 or
-             not self.joystick.is_connected() or self.joystick.get_axis_state('rx', -1) < 0.0):
+        if not self.joystick.get_button_state('tl', False) or not self.joystick.is_connected() or n_periods > self.command_rate_hz/4:
             self.speed = 0.0
             self.angular = 0.0
             self.right_motor.send_current_brake_command(brake_current)
@@ -203,19 +68,27 @@ class TractorController:
             return
         else:
             speed = np.clip(-self.joystick.get_axis_state('y', 0), -1.0, 1.0)
+            if speed < 0.5:
+                speed = speed/4.0
+            if speed >= 0.5:
+                speed = 0.5/4 + (speed - 0.5)*2
+            
             angular = np.clip(-self.joystick.get_axis_state('z', 0), -1.0, 1.0)
             if self.lock_out and (np.abs(speed) > 0.1 or np.abs(angular) > 0.1):
                 speed = 0.0
                 angular = 0.0
             else:
                 self.lock_out = False
-            speed = 1.5*speed
+            speed = speed
             angular = angular*np.pi/3.0
-        alpha = 0.02
-        delta_speed = np.clip(speed - self.speed, -alpha, alpha)
-        delta_angular = np.clip(angular - self.angular, -alpha, alpha)
-        self.speed = np.clip(self.speed + delta_speed, -1.5, 1.5)
-        self.angular = np.clip(self.angular + delta_angular, -np.pi/3.0, np.pi/3.0)
+            
+        alpha = 0.05
+        self.speed = self.speed * (1-alpha) + speed*alpha
+        self.angular = self.angular * (1-alpha) + angular*alpha
+        
+        self.speed = np.clip(speed, -1, 1)
+        self.angular = np.clip(angular, -np.pi/3.0, np.pi/3.0)
+        
         left, right = kinematics.unicycle_to_wheel_velocity(self.speed, self.angular)
         self.right_motor.send_velocity_command(right)
         self.left_motor.send_velocity_command(left)
@@ -226,7 +99,7 @@ def main():
     event_loop = asyncio.get_event_loop()
     controller = TractorController(event_loop)
     logger.info('Created controller %s', controller)
-    _ = Periodic(60, event_loop, lambda n_periods: plog.writer().flush())
+    _ = Periodic(5, event_loop, lambda n_periods: plog.writer().flush(), name='plog.flush')
     event_loop.run_forever()
 
 
