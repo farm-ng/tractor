78bee1d3ab22bcfe9ed5b69de6fbe833e01c7315
Ethan Rublee <ethan.rublee@gmail.com>
2020-08-06 23:28:18 -0700

    remote steering, weird mappings on joystick TODO fix joystick mappings.

--- python/farm_ng/ipc.py
+++ python/farm_ng/ipc.py
@@ -1,24 +1,26 @@
 # socket_multicast_receiver.py
+import asyncio
+import logging
 import socket
 import struct
 import sys
-from farm_ng_proto.tractor.v1.io_pb2 import Event, Announce
 
-from google.protobuf.text_format import MessageToString
-from google.protobuf.timestamp_pb2 import Timestamp
-import asyncio
-import logging
-from farm_ng.periodic import Periodic
 import farm_ng_proto.tractor.v1.io_pb2
 import farm_ng_proto.tractor.v1.motor_pb2
+import farm_ng_proto.tractor.v1.steering_pb2
 import farm_ng_proto.tractor.v1.tracking_camera_pb2
+from farm_ng.periodic import Periodic
+from farm_ng_proto.tractor.v1.io_pb2 import Announce
+from farm_ng_proto.tractor.v1.io_pb2 import Event
+from google.protobuf.text_format import MessageToString
+from google.protobuf.timestamp_pb2 import Timestamp
 
 logger = logging.getLogger('ipc')
 logger.setLevel(logging.INFO)
 
 
 # https://en.wikipedia.org/wiki/Multicast_address
-# adminstratively scoped: 239.0.0.0 to 239.255.255.255	
+# adminstratively scoped: 239.0.0.0 to 239.255.255.255
 _g_multicast_group = ('239.20.20.20', 10000)
 
 
@@ -26,7 +28,7 @@ def host_is_local(hostname, port):
     # https://gist.github.com/bennr01/7043a460155e8e763b3a9061c95faaa0
     """returns True if the hostname points to the localhost, otherwise False."""
     hostname = socket.getfqdn(hostname)
-    if hostname in ("localhost", "0.0.0.0"):
+    if hostname in ('localhost', '0.0.0.0'):
         return True
     localhost = socket.gethostname()
     localaddrs = socket.getaddrinfo(localhost, port)
@@ -36,7 +38,8 @@ def host_is_local(hostname, port):
             if rsockaddr[0] == sockaddr[0]:
                 return True
     return False
-        
+
+
 class EventBus:
     def __init__(self):
         self._multicast_group = _g_multicast_group
@@ -50,27 +53,26 @@ class EventBus:
     def send(self, event: Event):
         self._state[event.name] = event
         self._mc_send_sock.sendto(event.SerializeToString(), self._multicast_group)
-        
+
     def _send_recv(self):
         data, server = self._mc_send_sock.recvfrom(1024)
-        #print('received {!r} from {}'.format(
-        #data, server))
+        # print('received {!r} from {}'.format(
+        # data, server))
 
     def _bus_recv(self):
         data, address = self._mc_recv_sock.recvfrom(1024)
         if address[1] == self._mc_send_sock.getsockname()[1] and host_is_local(address[0], address[1]):
             return
-        
-        #socket.getaddrinfo(*address)
+
+        # socket.getaddrinfo(*address)
         event = Event()
         event.ParseFromString(data)
         self._state[event.name] = event
-        
-        #print('my send addr {} received {} bytes from {} event {}'.format(
-        #self._mc_send_sock.getsockname(),
-        #len(data), address, MessageToString(event, as_one_line=True)))
-        #self._mc_send_sock.sendto(b'ack %d'%len(data), address)
 
+        # print('my send addr {} received {} bytes from {} event {}'.format(
+        # self._mc_send_sock.getsockname(),
+        # len(data), address, MessageToString(event, as_one_line=True)))
+        #self._mc_send_sock.sendto(b'ack %d'%len(data), address)
 
     def _make_mc_recv_socket(self):
         # Create the socket
@@ -78,7 +80,7 @@ class EventBus:
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
         # Bind to the server address
-        sock.bind(('',self._multicast_group[1]))
+        sock.bind(('', self._multicast_group[1]))
 
         # Tell the operating system to add the socket to
         # the multicast group on all interfaces.
@@ -87,19 +89,18 @@ class EventBus:
         sock.setsockopt(
             socket.IPPROTO_IP,
             socket.IP_ADD_MEMBERSHIP,
-            mreq)
+            mreq,
+        )
         return sock
-
 
     def _send_announce(self):
         announce = Announce()
         self._send(announce)
-        
-        
+
     def _make_mc_send_socket(self):
         # Create the socket
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        
+
         # Set the time-to-live for messages to 1 so they do not
         # go past the local network segment.
         ttl = struct.pack('b', 1)
@@ -113,13 +114,16 @@ class EventBus:
     def log_state(self):
         logger.info('\n'.join([MessageToString(value, as_one_line=True) for value in self._state.values()]))
 
-            
+
 _g_event_bus = None
+
+
 def get_event_bus():
     global _g_event_bus
     if _g_event_bus is None:
         _g_event_bus = EventBus()
     return _g_event_bus
+
 
 def make_event(name: str, message, stamp: Timestamp = None) -> Event:
     event = Event()
@@ -139,8 +143,7 @@ def main():
     _ = Periodic(1, event_loop, lambda n_periods: event_bus.log_state())
 
     event_loop.run_forever()
-    
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
-    
--- python/farm_ng/steering.py
+++ python/farm_ng/steering.py
@@ -1,9 +1,14 @@
 import asyncio
 import logging
+import sys
+
+import numpy as np
+from farm_ng.ipc import get_event_bus
+from farm_ng.ipc import make_event
+from farm_ng.joystick import MaybeJoystick
 from farm_ng.periodic import Periodic
 from farm_ng_proto.tractor.v1.steering_pb2 import SteeringCommand
 from google.protobuf.text_format import MessageToString
-from farm_ng.ipc import get_event_bus, make_event
 
 logger = logging.getLogger('steering')
 logger.setLevel(logging.INFO)
@@ -11,9 +16,10 @@ logger.setLevel(logging.INFO)
 event_bus = get_event_bus()
 
 
-_g_message_name='steering'
+_g_message_name = 'steering'
 
-class SteeringClient(object):
+
+class SteeringClient:
     def __init__(self):
         self._latest_command = SteeringCommand()
         self._stop_command = SteeringCommand()
@@ -22,8 +28,7 @@ class SteeringClient(object):
         self._stop_command.velocity = 0.0
         self._stop_command.angular_velocity = 0.0
         self.lock_out = True
-        
-        
+
     def get_steering_command():
         event = event_bus.get_last_event(_g_message_name)
         if event is None:
@@ -40,11 +45,11 @@ class SteeringClient(object):
             if abs(self._latest_command.velocity) > 0.01 or abs(self._latest_command.angular_velocity) > 0.01:
                 return self._stop_command
             self.lockout = False
-            
-        return self._latest_state
-        
 
-class SteeringSenderJoystick(object):
+        return self._latest_state
+
+
+class SteeringSenderJoystick:
     def __init__(self):
         loop = asyncio.get_event_loop()
         self.joystick = MaybeJoystick('/dev/input/js0',  loop)
@@ -54,31 +59,33 @@ class SteeringSenderJoystick(object):
         self._command = SteeringCommand()
 
     def send(self, n_periods):
-        if not self.joystick.get_button_state('tl', False) or not self.joystick.is_connected() or n_periods > self.rate_hz/4:
+        if not self.joystick.get_button_state('tl2', False) or not self.joystick.is_connected() or n_periods > self.rate_hz/4:
             self._command.velocity = 0.0
             self._command.angular_velocity = 0.0
             self._command.brake = 1.0
             self._command.deadman = 0.0
         else:
-            self._command.deadman = self.joystick.get_button_state('tl', False) ? 1.0 : 0.0
+            self._command.deadman = 1.0 if self.joystick.get_button_state('tl2', False) else 0.0
             self._command.brake = 0.0
-            
+
             velocity = np.clip(-self.joystick.get_axis_state('y', 0), -1.0, 1.0)
             if abs(velocity) < 0.5:
                 velocity = velocity/4.0
             if abs(velocity) >= 0.5:
-                velocity = np.sign(velocity) *(0.5/4 + (abs(velocity) - 0.5)*2)
+                velocity = np.sign(velocity) * (0.5/4 + (abs(velocity) - 0.5)*2)
             self._command.velocity = velocity
-            
-            angular_velocity = np.clip(-self.joystick.get_axis_state('z', 0), -1.0, 1.0)*np.pi/3.0
+
+            angular_velocity = np.clip(-self.joystick.get_axis_state('rx', 0), -1.0, 1.0)*np.pi/3.0
             self._command.angular_velocity = angular_velocity
-        event_loop.send(make_event(_g_message_name, self._command))
+        event_bus.send(make_event(_g_message_name, self._command))
+
 
 def main():
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
     event_loop = asyncio.get_event_loop()
     steering_sender = SteeringSenderJoystick()
     event_loop.run_forever()
+
 
 if __name__ == '__main__':
     main()
