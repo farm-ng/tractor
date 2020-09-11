e339beefa656c6011c12495f0ae5c0253a685d91
Ethan Rublee <ethan.rublee@gmail.com>
2020-09-11 14:42:00 -0400

    Use path to trigger starts.

--- jetson/install.sh
+++ jetson/install.sh
@@ -9,6 +9,10 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
 done
 export SERVICE_DIR=$( cd "$( dirname "${SOURCE}" )" >/dev/null 2>&1 && pwd )
 
+# clean up old systemctl services
+rm -f /etc/systemd/system/multi-user.target.wants/tractor*
+rm -f /etc/systemd/system/tractor*
+
 mkdir -p /opt/farm_ng/systemd
 prefix=/opt/farm_ng make -C $SERVICE_DIR/uhubctl
 prefix=/opt/farm_ng make -C $SERVICE_DIR/uhubctl install
@@ -21,16 +25,13 @@ systemctl daemon-reload
 
 # start on boot always...
 systemctl enable tractor-bringup.service
-systemctl enable tractor-ready.path
+systemctl enable tractor.path
+systemctl enable tractor-steering.path
 systemctl enable tractor-camera.path
+systemctl enable tractor-webservices.path
 
-systemctl enable tractor-steering.service
-systemctl enable tractor.service
-systemctl enable tractor-camera.service
-systemctl enable tractor-webservices.service
-
-systemctl start tractor-ready.path
+systemctl start tractor.path
+systemctl start tractor-steering.path
 systemctl start tractor-camera.path
-systemctl start tractor-steering.service
-systemctl start tractor-bringup.service
-systemctl start tractor-webservices.service
+systemctl start tractor-webservices.path
+
