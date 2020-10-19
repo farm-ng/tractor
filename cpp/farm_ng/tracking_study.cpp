2dd9cbaa159a323712c43312acf19f4c7b6273ae
Ethan Rublee <ethan.rublee@gmail.com>
2020-10-19 00:09:50 -0700

    Working pretty well.

--- cpp/farm_ng/tracking_study.cpp
+++ cpp/farm_ng/tracking_study.cpp
@@ -100,8 +100,8 @@ class ImageLoader {
     cv::Mat frame;
     if (image.resource().content_type() == "video/mp4") {
       OpenVideo(image.resource());
-      LOG(INFO) << image.resource().path()
-                << " frame number: " << image.frame_number().value();
+      VLOG(2) << image.resource().path()
+              << " frame number: " << image.frame_number().value();
 
       int frame_number = image.frame_number().value();
       if (!FLAGS_zero_indexed) {
@@ -133,8 +133,11 @@ struct FlowPointImage {
 class FlowPointWorld {
  public:
   uint64_t id;
+
   Eigen::Vector3d point_world;
+  // set because we want it ordered.
   std::set<uint64_t> image_ids;
+  double rmse = 0;
 };
 
 class FlowImage {
@@ -268,64 +271,13 @@ struct PoseCostFunctor {
 typedef TimeSeries<BaseToCameraModel::WheelMeasurement>::RangeT
     WheelMeasurementRange;
 
-void SolvePoseDelta(const CameraModel& camera_model,
-                    const std::vector<cv::Point2f>& points_image_start,
-                    const std::vector<cv::Point2f>& points_image_end,
-                    Sophus::SE3d* camera_start_pose_camera_end,
-                    std::vector<double>* err) {
-  CHECK_EQ(points_image_start.size(), points_image_end.size());
-  err->clear();
-  err->resize(points_image_start.size());
+struct FlowBlock {
+  FlowImage* flow_image;
+  FlowPointWorld* flow_point_world;
+  FlowPointImage flow_point_image;
+};
 
-  auto presolve_camera_start_pose_camera_end = *camera_start_pose_camera_end;
-
-  ceres::Problem problem;
-  problem.AddParameterBlock(camera_start_pose_camera_end->data(),
-                            Sophus::SE3d::num_parameters,
-                            new LocalParameterizationSE3);
-  std::vector<Eigen::Vector3d> points_start, points_end;
-  for (size_t i = 0; i < points_image_start.size(); ++i) {
-    Eigen::Vector3d point_image_rect_start = ReprojectPixelToPoint(
-        camera_model,
-        Eigen::Vector2d(points_image_start[i].x, points_image_start[i].y), 1.0);
-    points_start.push_back(point_image_rect_start);
-    Eigen::Vector3d point_image_rect_end = ReprojectPixelToPoint(
-        camera_model,
-        Eigen::Vector2d(points_image_end[i].x, points_image_end[i].y), 1.0);
-    points_end.push_back(point_image_rect_end);
-    ceres::CostFunction* cost_function1 = new ceres::AutoDiffCostFunction<
-        EpipolarCostFunctor, 1, Sophus::SE3d::num_parameters>(
-        new EpipolarCostFunctor(point_image_rect_start, point_image_rect_end));
-
-    problem.AddResidualBlock(cost_function1, new ceres::CauchyLoss(0.005),
-                             camera_start_pose_camera_end->data());
-  }
-  // Set solver options (precision / method)
-  ceres::Solver::Options options;
-  options.linear_solver_type = ceres::DENSE_SCHUR;
-  options.gradient_tolerance = 1e-18;
-  options.function_tolerance = 1e-18;
-  options.parameter_tolerance = 1e-18;
-  options.max_num_iterations = 2000;
-
-  // Solve
-  ceres::Solver::Summary summary;
-  // options.logging_type = ceres::PER_MINIMIZER_ITERATION;
-  options.minimizer_progress_to_stdout = false;
-  ceres::Solve(options, &problem, &summary);
-  LOG(INFO) << summary.BriefReport();
-
-  VLOG(2) << "Presolve:\n"
-          << presolve_camera_start_pose_camera_end.matrix3x4()
-          << "\nPostsolve:\n"
-          << camera_start_pose_camera_end->matrix3x4();
-
-  for (size_t i = 0; i < points_start.size(); ++i) {
-    double distance = EpipolarDistance<double>(points_start[i], points_end[i],
-                                               *camera_start_pose_camera_end);
-    (*err)[i] = distance;
-  }
-}
+typedef std::unordered_map<uint64_t, std::vector<FlowBlock>> FlowBlocks;
 
 class FlowBookKeeper {
  public:
@@ -456,10 +408,12 @@ class FlowBookKeeper {
       // Crowding tends to occur when moving backwards,negatively along the
       // camera Z axis, as points that were close the camera get farther away
       // and closer together.
-      cv::rectangle(
-          crowding_mask,
-          cv::Rect(curr_points[i].x - 10, curr_points[i].y - 10, 20, 20),
-          cv::Scalar::all(255), -1);
+      int crowd_window = 41;
+      cv::rectangle(crowding_mask,
+                    cv::Rect(curr_points[i].x - crowd_window / 2,
+                             curr_points[i].y - crowd_window / 2, crowd_window,
+                             crowd_window),
+                    cv::Scalar::all(255), -1);
       prev_match.push_back(prev_points[i]);
       curr_match.push_back(curr_points[i]);
 
@@ -470,21 +424,7 @@ class FlowBookKeeper {
       curr_flow_points.push_back(flow_point);
     }
 
-#ifdef FIT_EPILINES
-    std::vector<double> epi_err;
-    Sophus::SE3d camera_prev_pose_camera_curr =
-        prev.camera_pose_world * flow_image->camera_pose_world.inverse();
-    SolvePoseDelta(camera_model_,
-
-                   prev_match, curr_match, &camera_prev_pose_camera_curr,
-                   &epi_err);
-#endif
     for (size_t i = 0; i < curr_flow_points.size(); ++i) {
-#ifdef FIT_EPILINES
-      if (epi_err[i] > 0.005) {
-        continue;
-      }
-#endif
       const auto& flow_point = curr_flow_points[i];
 
       flow_image->flow_points[flow_point.flow_point_id] = flow_point;
@@ -507,6 +447,20 @@ class FlowBookKeeper {
       debug_image = debug_image + flow_image->debug_trails;
       cv::imshow("debug flow", debug_image);
     }
+  }
+
+  void RemoveBlock(const FlowBlock& block) {
+    block.flow_point_world->image_ids.erase(block.flow_image->id);
+    block.flow_image->flow_points.erase(block.flow_point_world->id);
+  }
+
+  void RemovePointWorld(uint64_t id) {
+    auto it = flow_points_world_.find(id);
+    CHECK(it != flow_points_world_.end());
+    for (uint64_t image_id : it->second.image_ids) {
+      MutableFlowImage(image_id)->flow_points.erase(id);
+    }
+    flow_points_world_.erase(it);
   }
 
   void RenderMaskOfFlowPoints(const FlowImage& flow_image, cv::Mat* mask,
@@ -554,7 +508,7 @@ class FlowBookKeeper {
     double k = 0.04;
 
     cv::Mat mask = lens_exclusion_mask_.clone();
-    RenderMaskOfFlowPoints(*flow_image, &mask, 40);
+    RenderMaskOfFlowPoints(*flow_image, &mask, 80);
 
     // cv::imshow("mask", mask);
     /// Apply corner detection
@@ -638,7 +592,7 @@ class FlowBookKeeper {
 };
 
 void SavePly(std::string ply_path, const std::vector<Eigen::Vector3d>& points) {
-  LOG(INFO) << ply_path;
+  LOG(INFO) << ply_path << " npoints: " << points.size();
   std::ofstream out(ply_path);
   out << "ply\n";
   out << "format ascii 1.0\n";
@@ -699,53 +653,98 @@ class VisualOdometer {
     }
   }
 
-  void AddFlowImageToProblem(FlowImage* flow_image, ceres::Problem* problem) {
+  void AddFlowPointWorldToBlocks(FlowPointWorld* flow_point_world,
+                                 FlowBlocks* flow_blocks,
+                                 std::set<uint64_t>* flow_image_ids) {
+    if (flow_point_world->image_ids.size() < 5) {
+      return;
+    }
+    size_t n_images = flow_point_world->image_ids.size();
+    int skip = n_images / 5;
+    int count = 0;
+    for (auto image_id : flow_point_world->image_ids) {
+      if (count++ % skip != 0) {
+        continue;
+      }
+      FlowImage* flow_image = flow_.MutableFlowImage(image_id);
+      auto flow_point = flow_image->flow_points.find(flow_point_world->id);
+      CHECK(flow_point != flow_image->flow_points.end());
+      flow_image_ids->insert(flow_image->id);
+      (*flow_blocks)[flow_point_world->id].push_back(
+          {flow_image, flow_point_world, flow_point->second});
+    }
+  }
+
+  void AddFlowBlockToProblem(ceres::Problem* problem,
+                             const FlowBlock& flow_block) {
+    ceres::CostFunction* cost_function1 =
+        new ceres::AutoDiffCostFunction<ProjectionCostFunctor, 2,
+                                        Sophus::SE3d::num_parameters, 3>(
+            new ProjectionCostFunctor(
+                camera_model_,
+                flow_block.flow_point_image.image_coordinates.cast<double>()));
+
+    problem->AddParameterBlock(flow_block.flow_point_world->point_world.data(),
+                               3);
+    problem->AddResidualBlock(cost_function1, new ceres::CauchyLoss(2),
+                              flow_block.flow_image->camera_pose_world.data(),
+                              flow_block.flow_point_world->point_world.data());
+  }
+
+  void AddFlowImageToProblem(FlowImage* flow_image, ceres::Problem* problem,
+                             FlowBlocks* flow_blocks) {
     problem->AddParameterBlock(flow_image->camera_pose_world.data(),
                                Sophus::SE3d::num_parameters,
                                new LocalParameterizationSE3);
 
     for (const auto& id_flow_point : flow_image->flow_points) {
-      const auto& flow_point = id_flow_point.second;
+      const FlowPointImage& flow_point = id_flow_point.second;
+
       FlowPointWorld* flow_point_world =
           flow_.MutableFlowPointWorld(flow_point.flow_point_id);
       if (flow_point_world->image_ids.size() < 5) {
         continue;
       }
-      ceres::CostFunction* cost_function1 =
-          new ceres::AutoDiffCostFunction<ProjectionCostFunctor, 2,
-                                          Sophus::SE3d::num_parameters, 3>(
-              new ProjectionCostFunctor(
-                  camera_model_, flow_point.image_coordinates.cast<double>()));
-
-      problem->AddParameterBlock(flow_point_world->point_world.data(), 3);
-
-      problem->AddResidualBlock(cost_function1, new ceres::CauchyLoss(0.5),
-                                flow_image->camera_pose_world.data(),
-                                flow_point_world->point_world.data());
+      if (flow_blocks->size() < 250 ||
+          flow_blocks->count(flow_point_world->id)) {
+        FlowBlock flow_block({flow_image, flow_point_world, flow_point});
+        (*flow_blocks)[flow_point_world->id].push_back(flow_block);
+        AddFlowBlockToProblem(problem, flow_block);
+      }
     }
   }
   void SolvePose() {
     ceres::Problem problem;
 
     std::set<uint64_t> flow_image_ids;
-    std::unordered_map<uint64_t, FlowPointWorld*> flow_points_world;
-
-    uint64_t image_id(flow_.LastImageId());
-    if (image_id < 4) {
-      return;
-    }
-    int skip_n = 1;
-    while (flow_image_ids.size() < 5) {
-      FlowImage* flow_image = flow_.MutableFlowImage(image_id);
-      flow_image_ids.insert(image_id);
-      AddFlowImageToProblem(flow_image, &problem);
-      if (int64_t(image_id) - skip_n < 0) {
-        break;
+    FlowBlocks flow_blocks;
+    {
+      uint64_t image_id(flow_.LastImageId());
+      if (image_id < 5) {
+        return;
       }
-      image_id -= skip_n;
-      skip_n *= 2;
+      uint64_t begin_id = std::max(0, int(image_id) - 50);
+      flow_image_ids.insert(image_id);
+      if (goal_image_id_) {
+        if (int(image_id) - int(*goal_image_id_) < 50) {
+          flow_image_ids.insert(*goal_image_id_);
+        }
+      }
+      uint64_t skip = std::max(1, (int(image_id) - int(begin_id)) / 5);
+      while (begin_id < image_id) {
+        flow_image_ids.insert(begin_id);
+        begin_id += skip;
+      }
     }
-    CHECK_GE(flow_image_ids.size(), 2);
+    for (auto image_id : flow_image_ids) {
+      FlowImage* flow_image = flow_.MutableFlowImage(image_id);
+      AddFlowImageToProblem(flow_image, &problem, &flow_blocks);
+    }
+    std::stringstream ss;
+    for (auto id : flow_image_ids) {
+      ss << " " << id;
+    }
+    LOG(INFO) << "Num images: " << flow_image_ids.size() << ss.str();
 
     // auto start = flow_image_ids.begin();
     // auto end = ++flow_image_ids.begin();
@@ -777,20 +776,10 @@ class VisualOdometer {
                                flow_image_end->camera_pose_world.data());
     }
 
-    LOG(INFO) << "distance: " << flow_.LastImageId() - *flow_image_ids.begin();
-
-    // for (auto id : flow_image_ids) {
-    // problem.SetParameterBlockConstant(
-    // flow_.MutableFlowImage(flow_.LastImageId() - 1)
-    //->camera_pose_world.data());
-    // break;
-    //}
-
-    std::stringstream ss;
-    for (auto id : flow_image_ids) {
-      ss << " " << id;
+    if (false && goal_image_id_ && flow_image_ids.count(*goal_image_id_)) {
+      problem.SetParameterBlockConstant(
+          flow_.MutableFlowImage(*goal_image_id_)->camera_pose_world.data());
     }
-    LOG(INFO) << "Num images: " << flow_image_ids.size() << ss.str();
 
     // Set solver options (precision / method)
     ceres::Solver::Options options;
@@ -798,13 +787,58 @@ class VisualOdometer {
     options.gradient_tolerance = 1e-4;
     options.function_tolerance = 1e-4;
     options.parameter_tolerance = 1e-4;
-    options.max_num_iterations = 2000;
+    options.max_num_iterations = 200;
 
     // Solve
     ceres::Solver::Summary summary;
     // options.logging_type = ceres::PER_MINIMIZER_ITERATION;
     options.minimizer_progress_to_stdout = false;
     ceres::Solve(options, &problem, &summary);
+    LOG(INFO) << summary.BriefReport();
+
+    double all_rmse = 0;
+    double all_n = 0;
+    for (auto id_blocks : flow_blocks) {
+      uint64_t point_world_id = id_blocks.first;
+      double rmse = 0;
+      double n = 0;
+      for (auto block : id_blocks.second) {
+        Eigen::Vector3d point_camera = block.flow_image->camera_pose_world *
+                                       block.flow_point_world->point_world;
+        Eigen::Vector2d point_image_proj =
+            ProjectPointToPixel(camera_model_, point_camera);
+
+        if (point_camera.z() < 0) {
+          flow_.RemoveBlock(block);
+          continue;
+        }
+
+        if (point_camera.norm() > 5e2) {
+          flow_.RemoveBlock(block);
+          continue;
+        }
+        double err2 = (point_image_proj -
+                       block.flow_point_image.image_coordinates.cast<double>())
+                          .squaredNorm();
+        if (err2 > (4 * 4)) {
+          flow_.RemoveBlock(block);
+        } else {
+          rmse += err2;
+          n += 1;
+        }
+      }
+      auto flow_point_world = flow_.MutableFlowPointWorld(point_world_id);
+      if (flow_point_world->image_ids.size() < 5 ||
+          flow_point_world->point_world.norm() > 1e3) {
+        flow_.RemovePointWorld(point_world_id);
+      } else {
+        flow_.MutableFlowPointWorld(point_world_id)->rmse = std::sqrt(rmse / n);
+        all_rmse += rmse;
+        all_n += n;
+      }
+    }
+    LOG(INFO) << "RMSE: " << std::sqrt(all_rmse / std::max(all_n, 1.0))
+              << " N: " << all_n;
     // LOG(INFO) << summary.FullReport();
 
     /* for (auto id : flow_image_ids) {
@@ -814,15 +848,15 @@ class VisualOdometer {
     ceres::Solve(options, &problem, &summary); */
     // LOG(INFO) << "re solve:" << summary.FullReport();
 
-    FlowImage* flow_image = flow_.MutableFlowImage(flow_.LastImageId());
     cv::Mat reprojection_image;
-    cv::cvtColor(*flow_image->image, reprojection_image, cv::COLOR_GRAY2BGR);
+    FlowImage* flow_image = flow_.MutablePreviousFlowImage();
+    cv::cvtColor(*(flow_image->image), reprojection_image, cv::COLOR_GRAY2BGR);
 
     for (const auto& id_flow_point : flow_image->flow_points) {
       const auto& flow_point = id_flow_point.second;
       FlowPointWorld* flow_point_world =
           flow_.MutableFlowPointWorld(flow_point.flow_point_id);
-      if (flow_point_world->image_ids.size() < 5) {
+      if (flow_point_world->rmse == 0.0) {
         continue;
       }
       Eigen::Vector2d point_image_proj =
@@ -838,19 +872,71 @@ class VisualOdometer {
                  EigenToCvPoint(flow_point.image_coordinates), 5,
                  flow_.Color(flow_point.flow_point_id));
     }
+
+    if (goal_image_id_) {
+      auto base_pose_world_goal =
+          camera_pose_base_goal_.inverse() *
+          flow_.MutableFlowImage(*goal_image_id_)->camera_pose_world;
+
+      auto camera_pose_base_goal =
+          flow_image->camera_pose_world * base_pose_world_goal.inverse();
+
+      goal_image_id_ = flow_image->id;
+      camera_pose_base_goal_ = camera_pose_base_goal;
+      for (int i = 0; i < 1000; ++i) {
+        Eigen::Vector3d p1 =
+            camera_pose_base_goal * Eigen::Vector3d(0.1 * i, 0.0, 0.0);
+        Eigen::Vector3d p2 =
+            camera_pose_base_goal * Eigen::Vector3d(0.1 * (i + 1), 0.0, 0.0);
+        if (p1.z() > 0.0 && p2.z() > 0.00) {
+          cv::line(reprojection_image,
+                   EigenToCvPoint(ProjectPointToPixel(camera_model_, p1)),
+                   EigenToCvPoint(ProjectPointToPixel(camera_model_, p2)),
+                   cv::Scalar(0, 255, 0), 3);
+        }
+      }
+    }
+    auto camera_pose_base = base_pose_camera_.inverse();
+    for (int i = 0; i < 1000; ++i) {
+      Eigen::Vector3d ap1 =
+          camera_pose_base * Eigen::Vector3d(0.1 * i, 0.0, 0.0);
+      Eigen::Vector3d ap2 =
+          camera_pose_base * Eigen::Vector3d(0.1 * (i + 1), 0.0, 0.0);
+      cv::line(reprojection_image,
+               EigenToCvPoint(ProjectPointToPixel(camera_model_, ap1)),
+               EigenToCvPoint(ProjectPointToPixel(camera_model_, ap2)),
+               cv::Scalar(0, 0, 255), 1);
+    }
+
+    cv::flip(reprojection_image, reprojection_image, -1);
+    if (!writer) {
+      writer.reset(new cv::VideoWriter(
+          video_writer_dev,
+          0,   // fourcc
+          30,  // fps
+          cv::Size(camera_model_.image_width(), camera_model_.image_height()),
+          true));
+    }
+    writer->write(reprojection_image);
     cv::imshow("reprojection", reprojection_image);
   }
 
   void DumpFlowPointsWorld(std::string ply_path) {
     std::vector<Eigen::Vector3d> points;
     for (auto it : flow_.FlowPointsWorld()) {
-      if (it.second.image_ids.size() > 30) {
-        if (it.second.point_world.norm() < 500) {
-          points.push_back(it.second.point_world);
-        }
+      if (it.second.image_ids.size() >= 5) {
+        points.push_back(it.second.point_world);
       }
     }
     SavePly(ply_path, points);
+  }
+
+  void SetGoal() {
+    if (flow_.LastImageId() < 10) {
+      return;
+    }
+    goal_image_id_ = flow_.LastImageId();
+    camera_pose_base_goal_ = base_pose_camera_.inverse();
   }
 
  private:
@@ -860,7 +946,16 @@ class VisualOdometer {
 
   Sophus::SE3d base_pose_camera_;
   Sophus::SE3d odometry_pose_base_;
+
+  std::optional<uint64_t> goal_image_id_;
+  Sophus::SE3d camera_pose_base_goal_;
   TimeSeries<BaseToCameraModel::WheelMeasurement> wheel_measurements_;
+
+  std::string video_writer_dev =
+      std::string("appsrc !") + " videoconvert ! x264enc ! " +
+      " mp4mux ! filesink location=" + "/tmp/out.mp4";
+
+  std::unique_ptr<cv::VideoWriter> writer;
 };
 
 class TrackingStudyProgram {
@@ -904,6 +999,7 @@ class TrackingStudyProgram {
     std::unique_ptr<VisualOdometer> vo;
     ImageLoader image_loader;
 
+    int skip_frame_counter = 0;
     while (true) {
       try {
         auto event = log_reader.ReadNext();
@@ -917,7 +1013,7 @@ class TrackingStudyProgram {
           }
         }
         Image image;
-        if (event.data().UnpackTo(&image)) {
+        if (event.data().UnpackTo(&image) && skip_frame_counter++ % 2 == 0) {
           if (!vo) {
             vo.reset(
                 new VisualOdometer(image.camera_model(), base_to_camera_model));
@@ -934,7 +1030,13 @@ class TrackingStudyProgram {
                            now - last_stamp)
                     << " ms";
 
-          cv::waitKey(1);
+          int key = cv::waitKey(1) & 0xff;
+          if (key == ' ') {
+            vo->SetGoal();
+          }
+          if (key == 'q') {
+            break;
+          }
         }
         bus_.get_io_service().poll();
 
@@ -945,6 +1047,8 @@ class TrackingStudyProgram {
         break;
       }
     }
+
+    vo->DumpFlowPointsWorld("/tmp/flow_points_world.final.ply");
     return 0;
     std::optional<google::protobuf::Timestamp> last_image_stamp;
 
