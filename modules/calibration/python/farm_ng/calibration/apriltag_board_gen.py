fd63ee3c0b8c0832141abe633d529a49f917d4d6
Ethan Rublee <ethan.rublee@gmail.com>
2020-11-22 10:13:29 -0800

    Add apriltag_board_gen, pre-generated config files for standard boards, and documentation.

--- modules/calibration/python/farm_ng/calibration/apriltag_board_gen.py
+++ modules/calibration/python/farm_ng/calibration/apriltag_board_gen.py
@@ -1,13 +1,13 @@
 # python -m farm_ng.apriltag_board_gen --root_tag_id 1 --name intrinsic_board_01
-
+# python -m farm_ng.calibration.apriltag_board_gen
 import argparse
 
 import google.protobuf.json_format as json_format
 from liegroups import SE3
 
-from farm_ng.proto_utils import se3_to_proto
-from farm_ng_proto.tractor.v1.apriltag_pb2 import ApriltagRig
-from farm_ng_proto.tractor.v1.geometry_pb2 import Vec3
+from farm_ng.perception.apriltag_pb2 import ApriltagRig
+from farm_ng.perception.geometry_pb2 import Vec3
+from farm_ng.perception.proto_utils import se3_to_proto
 
 
 def MakeApriltagRigNode(rig_name, tag_id, size, x, y):
@@ -29,11 +29,12 @@ def MakeApriltagRigNode(rig_name, tag_id, size, x, y):
 def App():
     parser = argparse.ArgumentParser()
     parser.add_argument('--name', type=str, default='tag_rig')
-    parser.add_argument('--tag_size', type=float, default=0.1)
+    parser.add_argument('--tag_size', type=float, default=0.044)
+    parser.add_argument('--tag_spacing', type=float, default=0.056)
 
     parser.add_argument('--root_tag_id', type=int, default=1)
-    parser.add_argument('--num_rows', type=int, default=3)
-    parser.add_argument('--num_cols', type=int, default=2)
+    parser.add_argument('--num_rows', type=int, default=11)
+    parser.add_argument('--num_cols', type=int, default=7)
 
     args = parser.parse_args()
 
@@ -41,10 +42,10 @@ def App():
     rig.name = args.name
 
     tag_id = args.root_tag_id
-    for x in range(args.num_cols):
-        for y in range(args.num_rows):
-            x_meters = x*args.tag_size*1.2
-            y_meters = y*args.tag_size*1.2
+    for y in range(args.num_rows):
+        for x in range(args.num_cols):
+            x_meters = x*args.tag_spacing
+            y_meters = y*args.tag_spacing
             # center to center
             rig.nodes.append(MakeApriltagRigNode(args.name, tag_id, args.tag_size, x_meters, y_meters))
             tag_id += 1
