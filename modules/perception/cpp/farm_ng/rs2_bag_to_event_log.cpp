1ff1fc0b5cc25aba8e303d8f882fdaafc4b54b9a
Collin Brake <collinmbrake@gmail.com>
2020-12-28 14:39:36 -0500

    start

--- modules/perception/cpp/farm_ng/rs2_bag_to_event_log.cpp
+++ modules/perception/cpp/farm_ng/rs2_bag_to_event_log.cpp
@@ -1,3 +1,11 @@
+/* TODO:
+ * factor out intel utils to another source file
+ * decide whether to ouput video or jpegs
+ * populate depthmap field of image resource
+ * support interactive mode from the browser
+ * chunk the video files as in video_streamer.cpp if we decide to
+ */
+
 #include <iostream>
 #include <optional>
 
@@ -8,10 +16,10 @@
 #include "farm_ng/core/ipc.h"
 
 #include "farm_ng/perception/camera_model.h"
-#include "farm_ng/perception/intel_rs2_utils.h"
 #include "farm_ng/perception/camera_pipeline.pb.h"
-#include "farm_ng/perception/rs2_bag_to_eventlog.pb.h"
 #include "farm_ng/perception/image.pb.h"
+#include "farm_ng/perception/intel_rs2_utils.h"
+#include "farm_ng/perception/rs2_bag_to_eventlog.pb.h"
 
 DEFINE_bool(interactive, false, "receive program args via eventbus");
 DEFINE_string(name, "default",
@@ -37,26 +45,20 @@ class Rs2BagToEventLogProgram {
                             Rs2BagToEventLogConfiguration configuration,)
                             bool interactive)
       : bus_(bus), timer_(bus_.get_io_service()) {
-    if (interactive) { // The program doesn't use interactive mode at this point
+    if (interactive) {  // The program doesn't use interactive mode at this
+                        // point
       status_.mutable_input_required_configuration()->CopyFrom(configuration);
     } else {
       set_configuration(configuration);
     }
     bus_.AddSubscriptions({bus_.GetName()});
-
-    /*
-    bus_.GetEventSignal()->connect(std::bind(
-        &Rs2BagToEventLogProgram::on_event, this, std::placeholders::_1));
-    on_timer(boost::system::error_code());
-    */
   }
 
   int run() {
-
     Rs2BagToEventLogResult result;
 
     // We prly don't need begin/end timestamps
-    //result.mutable_stamp_begin()->CopyFrom(MakeTimestampNow());
+    result.mutable_stamp_begin()->CopyFrom(MakeTimestampNow());
 
     // Start a realsense pipeline from a recorded file to get the framesets
     rs2::pipeline pipe;
@@ -65,33 +67,53 @@ class Rs2BagToEventLogProgram {
     cfg.enable_device_from_file(configuration_.bag_file_name());
     rs2::video_stream_profile profile = pipe.start(cfg);
 
+    CameraModel camera_model;
+    camera_model.set_frame_name(configuration_.camera_frame_name());
+    SetCameraModelFromRs(camera_model, profile.get_intrinsics());
+
     Image image_pb;
     image_pb.mutable_fps()->set_value(30);
+    image_pb.mutable_camera_model()->CopyFrom(*camera_model);
     image_pb.mutable_frame_number()->set_value(0);
+
+    encoder = " x264enc ! ";
+    auto resource_path = GetUniqueArchiveResource(
+        image_pb.camera_model().frame_name(), "mp4", "video/mp4");
+    gst_pipeline =
+        std::string("appsrc !") + " videoconvert ! " + encoder +
+        " mp4mux ! filesink location=" + resource_path.second.string();
+
+    image_pb.mutable_resource()->CopyFrom(resource_path.first);
+    cv::VideoWriter writer =
+        cv::VideoWriter(gst_pipeline,
+                        0,                       // fourcc
+                        image_pb.fps().value(),  // fps
+                        cv::Size(image_pb.camera_model().image_width(),
+                                 image_pb.camera_model().image_height()),
+                        true);
 
     LoggingStatus log = StartLogging(bus_, configuration_.name());
 
-    int frame_count = 0;
-
     while (true) {
-
       // Fetch the next frameset (block until it comes)
       rs2::frameset frames = pipe.wait_for_frames().as<rs2::frameset>();
-      ++frame_count;
 
       // Get the depth and color frames
       rs2::video_frame color_frame = frames.get_color_frame();
-      //rs2::depth_frame depth_frame = frames.get_depth_frame();
+      // rs2::depth_frame depth_frame = frames.get_depth_frame();
 
       // Query frame size (width and height)
       const int wc = color_frame.get_width();
       const int hc = color_frame.get_height();
-      //const int wd = depth_frame.get_width();
-      //const int hd = depth_frame.get_height();
+      // const int wd = depth_frame.get_width();
+      // const int hd = depth_frame.get_height();
+
+      CHECK_EQ(camera_model->image_width(), wc);
+      CHECK_EQ(camera_model->image_height(), hc);
 
       // Image matrices from rs2 frames
       cv::Mat color = RS2FrameToMat(color_frame);
-      //cv::Mat depth = RS2FrameToMat(depth_frame),
+      // cv::Mat depth = RS2FrameToMat(depth_frame),
 
       if (color.empty()) {
         break;
@@ -99,6 +121,7 @@ class Rs2BagToEventLogProgram {
 
       // Display the current video frame, using case insensitive q as quit
       // signal.
+      /*
       cv::imshow("Color", color);
       char c = static_cast<char>(cv::waitKey(1));
       if (tolower(c) == 'q')
@@ -106,38 +129,29 @@ class Rs2BagToEventLogProgram {
          cout << "Quit signal recieved, stopping video.\n\n";
          break;
       }
+      */
 
-      std::optional<CameraModel> camera_model;
-      if (!camera_model) {
-        // Do we need to define a new CameraModel instance and assign it to
-        // camera_model as in DefaultCameraModel()?
-        camera_model.set_frame_name(camera_frame_name)
-        SetCameraModelFromRs(camera_model, profile.get_intrinsics())
-        image_pb.mutable_camera_model()->CopyFrom(*camera_model);
-      }
-
-      CHECK_EQ(camera_model->image_width(), w);
-      CHECK_EQ(camera_model->image_height(), h);
-
-      // Copy the data to the image protobuf, TODO
-      // How do we actualy serialize the image?
-      // Cannot find this in create_video_dataset.
-      //image_pb.mutable_resource->CopyFrom(...)
+      writer.write(color);
 
       // Send out the image Protobuf on the event bus
       auto stamp = core::MakeTimestampNow();
       bus_.Send(
           MakeEvent(camera_model->frame_name() + "/image", image_pb, stamp));
 
-      result.mutable_configuration()->CopyFrom(configuration_);
-      result.mutable_frames()->set_path(log.recording().archive_path());
-
-      ArchiveProtobufAsJsonResource(configuration_.name(), result);
+      // zero index base for the frame_number, set after send.
+      image_pb.mutable_frame_number()->set_value(
+          image_pb.frame_number().value() + 1);
     }
+
+    result.mutable_configuration()->CopyFrom(configuration_);
+    result.mutable_dataset()->CopyFrom(image_pb.resource());
+    result.mutable_stamp_end()->CopyFrom(MakeTimestampNow());
+
+    ArchiveProtobufAsJsonResource(configuration_.name(), result);
 
     LOG(INFO) << "Complete:\n" << status_.DebugString();
 
-    status_.set_num_frames(frame_count);
+    status_.set_num_frames(image_pb.frame_number().value());
     send_status();
     return 0;
   }
@@ -168,7 +182,7 @@ int Main(farm_ng::core::EventBus& bus) {
   config.mutable_rs2_bag_resource().set_content_type("rs2bag");
 
   farm_ng::perception::Rs2BagToEventLogProgram program(bus, config,
-                                                         FLAGS_interactive);
+                                                       FLAGS_interactive);
   return program.run();
 }
 
