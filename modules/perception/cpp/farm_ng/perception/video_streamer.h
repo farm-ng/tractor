10b37d4261a7b78b6cf42408d05185ce8c4c618d
Collin Brake <collinmbrake@gmail.com>
2020-12-31 13:51:33 -0500

    depth map mode

--- modules/perception/cpp/farm_ng/perception/video_streamer.h
+++ modules/perception/cpp/farm_ng/perception/video_streamer.h
@@ -17,9 +17,16 @@ class VideoStreamer {
   enum Mode {
     MODE_UNDEFINED = 0,
     MODE_PNG_SEQUENCE = 1,
-    MODE_MP4_FILE = 2,
-    MODE_MP4_UDP = 3
+    MODE_JPG_SEQUENCE = 2,
+    MODE_MP4_FILE = 3,
+    MODE_MP4_UDP = 4,
+    MODE_DEPTH = 5
   };
+  enum DepthMode {
+    DEPTH_MODE_UNDEFINED = 0,
+    DEPTH_MODE_LINEAR_16BIT_PNG = 1,
+    DEPTH_MODE_LINEAR_8BIT_JPG = 2
+  }
 
   VideoStreamer(farm_ng::core::EventBus& bus, const CameraModel& camera_model,
                 Mode mode, uint64_t port = 0);
@@ -28,18 +35,19 @@ class VideoStreamer {
                  const google::protobuf::Timestamp& stamp);
   Image AddFrameWithDepthmap(const cv::Mat& image, const cv::Mat& depthmap,
                              const google::protobuf::Timestamp& stamp);
+)
   void Close();
 
- private:
-  void ResetVideoStreamer(bool is_color);
+private:
+void ResetVideoStreamer(bool is_color);
 
-  farm_ng::core::EventBus& bus_;
-  Mode mode_;
-  Image image_pb_;
-  std::shared_ptr<cv::VideoWriter> writer_;
-  std::shared_ptr<cv::VideoWriter> writer_depthmap_;
-  const uint k_max_frames_ = 300;
-  uint64_t port_;
+farm_ng::core::EventBus& bus_;
+Mode mode_;
+Image image_pb_;
+std::shared_ptr<cv::VideoWriter> writer_;
+std::shared_ptr<cv::VideoWriter> writer_depthmap_;
+const uint k_max_frames_ = 300;
+uint64_t port_;
 };
 
 }  // namespace perception
