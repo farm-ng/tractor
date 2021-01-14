83d3168494d410908847963cc78e9903e98f76aa
Ethan Rublee <ethan.rublee@gmail.com>
2021-01-14 14:07:31 -0800

    mostly working user devel env.

--- devel.sh
+++ devel.sh
@@ -1,8 +1,4 @@
 #!/bin/bash -ex
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-TAG=farmng/devel@sha256:15c31ddbeb3649575de3dfbc4c3360c1b5af04e04fbc275fd7c9de4cde60b89d
-
 COMMAND=$@
-docker run \
-       --rm -v $DIR:/workspace/tractor:rw,Z --workdir /workspace/tractor \
-        $TAG ./env.sh $COMMAND
+uid=`id -u` gid=`id -g` docker-compose -f docker/devel/docker-compose.yml run --rm workspace $COMMAND
