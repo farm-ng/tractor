6c09826e6333f9f0273fd88d0fc85ca221e0a0df
Ethan Rublee <ethan.rublee@gmail.com>
2021-01-15 23:55:46 -0800

    Fix.

--- devel.sh
+++ devel.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-FARM_NG_DEVEL_IMAGE=${FARM_NG_DEVEL_IMAGE:-"farmng/devel@$(shell cat $DIR/docker/devel/devel-digest.txt)"}
+FARM_NG_DEVEL_IMAGE=${FARM_NG_DEVEL_IMAGE:-"farmng/devel@$(cat $DIR/docker/devel/devel-digest.txt)"}
 BLOBSTORE_ROOT=${BLOBSTORE_ROOT:-$DIR/../tractor-data}
 make FARM_NG_DEVEL_IMAGE=$FARM_NG_DEVEL_IMAGE \
      BLOBSTORE_ROOT=$BLOBSTORE_ROOT \
-     -C $DIR/docker/devel upd > /dev/null
+     -C $DIR/docker/devel upd
 bash_args=$@
 if [[ -z "$bash_args" ]] ; then
     bash_args="bash"
